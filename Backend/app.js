7d11bceaa44cd0659ac9a9de17cf18280f068357
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-12 15:22:25 +0530

    Added DB Integration

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const PORT = 3000
 const app = express()
+
+
+mongoose.connect('mongodb://root:root1234@localhost:27017/VocalTix?authSource=admin').then(() => {
+    app.listen(PORT, () => {
+        console.log('listening on port 3000')
+    })
+})
 
 app.get('/', (req, res) => {
     res.status(200).json({
         status: 'success',
         message: 'Page Loaded'
     })
-})
-
-app.listen(PORT, () => {
-    console.log('listening on port 3000')
 })
--- Backend/controllers/addHotel.js
+++ Backend/controllers/addHotel.js
@@ -3,6 +3,11 @@ const { Hotel } = require('./../models/hotel')
 exports.addHotel = async (req, res) => {
     const { name, location, destination, price, rating, roomType } = req.body
 
+    if(!name, !location, !destination, !price, !roomType, !rating) {
+        console.log('Please provide details')
+        return;
+    }
+
     const newHotel = await Hotel.create({ 
         name: name,
         location: location,
