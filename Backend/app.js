4c8f7308b5ff7c9f5e5370da8a75b9f52e86a96d
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-12 14:55:07 +0530

    Model Created

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,12 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Page Loaded'
+    })
+})
 
 app.listen(PORT, () => {
     console.log('listening on port 3000')
---
+++ Backend/models/hotel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const hotelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    roomTypes: {
+        type: String,
+        required: true
+    }
+});
+
+const Hotel = mongoose.model('Hotel', hotelSchema);
+
+module.exports = Hotel;
